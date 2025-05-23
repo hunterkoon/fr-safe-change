c8ec4d676eccd34ae0c3280762b874eb8c035f8e
Gabriel Campos <gabrielcampos.dgr@hotmail.com>
2025-05-22 22:36:31 -0300

    adicionando elementos home

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,7 +1,12 @@
 import {
-  trigger, transition, style, animate, query, group
+  trigger,
+  transition,
+  style,
+  animate,
+  query,
+  group,
 } from '@angular/animations';
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component, AfterViewInit } from '@angular/core';
 import { HeaderComponent } from '../layout/header/header.component';
 import { FooterComponent } from '../layout/footer/footer.component';
 import { RouterOutlet } from '@angular/router';
@@ -15,24 +20,47 @@ import { RouterOutlet } from '@angular/router';
   animations: [
     trigger('routeAnimations', [
       transition('* <=> *', [
-        query(':enter, :leave', [
-          style({ position: 'absolute', width: '100%', opacity: 0 })
-        ], { optional: true }),
+        query(
+          ':enter, :leave',
+          [style({ position: 'absolute', width: '100%', opacity: 0 })],
+          { optional: true }
+        ),
         group([
-          query(':leave', [
-            animate('200ms ease', style({ opacity: 0, transform: 'translateX(-100%)' }))
-          ], { optional: true }),
-          query(':enter', [
-            style({ transform: 'translateX(100%)', opacity: 0 }),
-            animate('200ms ease', style({ transform: 'translateX(0)', opacity: 1 }))
-          ], { optional: true })
-        ])
-      ])
-    ])
-  ]
+          query(
+            ':leave',
+            [
+              animate(
+                '200ms ease',
+                style({ opacity: 0, transform: 'translateX(-100%)' })
+              ),
+            ],
+            { optional: true }
+          ),
+          query(
+            ':enter',
+            [
+              style({ transform: 'translateX(100%)', opacity: 0 }),
+              animate(
+                '200ms ease',
+                style({ transform: 'translateX(0)', opacity: 1 })
+              ),
+            ],
+            { optional: true }
+          ),
+        ]),
+      ]),
+    ]),
+  ],
 })
-export class MainComponent {
-  prepareRoute(outlet: RouterOutlet) {
-    return outlet?.activatedRouteData?.['animation'];
+
+export class MainComponent implements AfterViewInit {
+   constructor(private cdr: ChangeDetectorRef) {}
+
+  ngAfterViewInit(): void {
+    this.cdr.detectChanges();
+  }
+
+  prepareRoute(outlet: RouterOutlet): string | null {
+    return outlet?.activatedRouteData?.['animation'] || null;
   }
 }
---
+++ src/app/shared/inputs/input-common/input-common.component.ts
@@ -0,0 +1,51 @@
+import { Component, Input, forwardRef } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
+
+@Component({
+  selector: 'app-input-common',
+  standalone: true,
+  imports: [],
+  templateUrl: './input-common.component.html',
+  styleUrl: './input-common.component.scss',
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => InputCommonComponent),
+      multi: true
+    }
+  ]
+})
+export class InputCommonComponent implements ControlValueAccessor {
+  @Input() id: string = '';
+  @Input() name: string = '';
+  @Input() type: string = '';
+  @Input() label: string = '';
+  @Input() placeholder: string = '';
+
+  value: string = '';
+  isDisabled = false;
+  onChange = (_: any) => {};
+  onTouched = () => {};
+
+  writeValue(value: any): void {
+    this.value = value;
+  }
+
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: any): void {
+    this.onTouched = fn;
+  }
+
+  setDisabledState(isDisabled: boolean): void {
+    this.isDisabled = isDisabled;
+  }
+
+  onInputChange(event: Event): void {
+    const input = event.target as HTMLInputElement;
+    this.value = input.value;
+    this.onChange(this.value);
+  }
+}
