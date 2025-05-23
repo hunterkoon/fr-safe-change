c6f8033999e8c1dd861b67abbbd617a9c9c53a5d
Gabriel Campos <66956341+hunterkoon@users.noreply.github.com>
2025-05-23 12:07:56 -0300

    Merge branch 'develop'

--- src/app/shared/constants/constants.ts
+++ src/app/shared/constants/constants.ts
@@ -1,2 +1,2 @@
-  export const LOGIN_IMG : string = "assets/images/home.png"
-  export const REGISTER_IMG : string = "assets/images/register.png"
+  export const LOGIN_IMG : string = "/assets/images/home.png"
+  export const REGISTER_IMG : string = "/assets/images/register.png"
